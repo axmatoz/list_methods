5eea651cb06e24e82d06a02085f52be53c4609c8
axmatoz <axmatozodbek@gmail.com>
2023-03-14 14:01:16 +0500

    list_method08.dart

--- bin/list_method08.dart
+++ bin/list_method08.dart
@@ -6,5 +6,22 @@
     Returns:
         list: return answer
 */
+func(List fruits){
+  int i = 0;
+  int a = 0;
+  List answer = [];
+  while (i < fruits.length){
+    if (fruits[i] == 'apple'){
+      answer.add(i);
+      a ++;
+    }
+    i ++;
+  }
+  answer.insert(0, a);
+  return answer;
+}
+void main() {
+    print(func(['peach','kiwi','apple','apple','orange','apple']));
+}
 
-void main() {}
+
