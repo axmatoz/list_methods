d7cccd6ae9c3dc7adfda8474df8d78c0f1b232b9
axmatoz <axmatozodbek@gmail.com>
2023-03-14 14:02:10 +0500

    list_method09.dart

--- bin/list_method09.dart
+++ bin/list_method09.dart
@@ -6,5 +6,21 @@
     Returns:
         list: return answer
 */
+List func(List list1) {
+  int i = 0;
+  int a = 0;
+  int b = 0;
+  while (i < list1.length) {
+    if (list1[i] == 1) {
+      a++;
+    } else {
+      b++;
+    }
+    i++;
+  }
+  return [a] + [b];
+}
 
-void main() {}
+void main() {
+  print(func([1, 0, 1, 1, 0, 0, 0, 1]));
+}
